0e74acdf8e4d82c8b5941d1aa9041aa13454121d
hpehliva <phlvnhalime@outlook.com>
2026-01-12 17:24:14 +0100

    feat: configured changed for wordpress and added a new packages for comments part of wordpress

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -18,17 +18,22 @@ if [ ! -f index.php ]; then
     --admin_email=$ADMIN_EMAIL \
     --allow-root
 
-    wp user create \
-    $USER_LOGIN \
-    $USER_EMAIL \
-    --role=author \
-    --user_pass=$USER_PASS \
-    --allow-root
 
+    wp user create "$USER_LOGIN" "$USER_EMAIL" \
+    --role=author \
+    --user_pass=$(cat /run/secrets/user_password)\
+    --allow-root
+    
+    wp config set WP_DEBUG true --raw --allow-root 
+    wp config set WP_DEBUG_DISPLAY true --raw --allow-root
+    wp config set WP_DEBUG_LOG true --raw --allow-root
 fi
 
 # Find and start PHP-FPM
-PHP_FPM=$(which php-fpm8 || which php-fpm || find /usr -name php-fpm* -type f 2>/dev/null | head -1)
+#PHP_FPM=$(which php-fpm8 || which php-fpm || find /usr -name php-fpm* -type f 2>/dev/null | head -1)
+PHP_FPM=$(which php-fpm82 2>/dev/null || which php-fpm8 2>/dev/null || which php-fpm 2>/dev/null)
+
+
 if [ -z "$PHP_FPM" ]; then
     echo "Error: php-fpm not found"
     exit 1
